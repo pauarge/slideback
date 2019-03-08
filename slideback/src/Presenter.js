8826308a5b57e6670c2c040a6a3a9952ddaec5f4
Pau Argelaguet <pau@outlook.com>
2019-03-09 00:11:32 +0100

    Updating bootstrap

--- slideback/src/Presenter.js
+++ slideback/src/Presenter.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
 
 class Presenter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numPages: null,
+      pageNumber: 1,
+    }
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+
   render() {
+    const { pageNumber, numPages } = this.state;
+
     return (
-      <p>Hello from presenter</p>
-    )
+      <div>
+        <Document
+          file={process.env.PUBLIC_URL + "./Report_hw2_bolon.pdf"}
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
   }
 }
 
--- slideback/src/index.js
+++ slideback/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
