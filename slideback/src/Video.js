c2c6cc0ab80279643fa9b7f6efc2415d7928f80e
Sourabh <sourabhlal@gmail.com>
2019-03-09 00:40:47 -0800

    worked on video frame extraction and MS api call

--- slideback/src/Video.js
+++ slideback/src/Video.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Webcam from "react-webcam";
+import request from 'superagent';
+import base64Img from 'base64-img';
+
+
+
 
 class Video extends React.Component {
 
   state = {
     imageData: null,
-    imageName: "",
-    imageSave: false
+    intervalID: null,
+    analysisUrl: null,
   }
 
-  intervalID = null
+  
 
   setRef = (webcam) => {
     this.webcam = webcam;
@@ -20,15 +25,38 @@ class Video extends React.Component {
     this.setState({
       imageData: imageSrc
     })
+    base64Img.img(this.state.imageData, './', 'test', function(err, filepath) {});
+    // this.processImage()
   };
 
 
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.capture(),
-      1000
-    );
+  processImage() {
+        const subscriptionKey = "4787f1decd6c451eb365b52b7092151c";
 
+        this.setState({
+          analysisUrl: this.refs.currentImage,
+        })
+
+       request
+         .post('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&returnFaceAttributes=headPose')
+         // putting an actualy url like "https://www.thoughtco.com/thmb/08sd14jZzhDl5nX4Qy0xqj82nUc=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-141090015-5ad4786efa6bcc0036b494de.jpg" works
+         .send({ "url": "https://www.thoughtco.com/thmb/08sd14jZzhDl5nX4Qy0xqj82nUc=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-141090015-5ad4786efa6bcc0036b494de.jpg" })
+         .set('Ocp-Apim-Subscription-Key', subscriptionKey)
+         .set('Content-Type', 'application/json')
+         .then(res => {
+            alert('yay got ' + JSON.stringify(res.body));
+         });
+
+}
+
+
+
+
+  componentDidMount() {
+    this.state.intervalID = setInterval(
+      () => this.capture(),
+      10000
+    );
 
   }
 
@@ -56,7 +84,7 @@ class Video extends React.Component {
 
       {this.state.imageData? 
         <p>
-          <img src={this.state.imageData}/>
+          <img ref="currentImage" src={this.state.imageData}/>
         </p>
         : null}
 
