6a4fe453d33e28fa3beef381888edfb26b86f8a9
Natalie Bolon <natalie.bolonbrun@epfl.ch>
2019-03-09 09:25:32 +0100

    changes on ditribution of content

--- slideback/src/Presenter.js
+++ slideback/src/Presenter.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {Document, Page} from 'react-pdf';
-import {Button, ButtonToolbar, Alert, ProgressBar} from "react-bootstrap";
+import {Button, ButtonToolbar, Alert, ProgressBar, Container, Row, Col} from "react-bootstrap";
 import io from "socket.io-client";
 
 import { SOCKET_URL } from './config';
+
 
 class Presenter extends Component {
   constructor(props) {
@@ -71,32 +72,67 @@ class Presenter extends Component {
     const now = 60;
 
     return (
-      <div>
-        <div className="button">
-          <ButtonToolbar>
-            <Button onClick={() => this.nextPage()} as="input" type="button" value=" Next "/>
-            <Button onClick={() => this.prevPage()} as="input" type="button" value=" Prev "/>
-          </ButtonToolbar>
-        </div>
+        <Container>
+          <Row>
 
-        <Alert defaultShow={this.state.alertFirst} variant="info first">
-          <Alert.Heading>You are already in the first page of the document</Alert.Heading>
-        </Alert>
+          <Col sm={6}>
 
-        <Alert defaultShow={this.state.alertLast} variant="info last">
-          <Alert.Heading>You are already in the last page of the document</Alert.Heading>
-        </Alert>
 
-        <Document
-          file={process.env.PUBLIC_URL + "./Report_hw2_bolon.pdf"}
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber}/>
-        </Document>
-        <p>Page {pageNumber} of {numPages}</p>
-        <p>Satisfaction level</p>
-        <ProgressBar now={now} label={`${now}%`} />
-      </div>
+            <Document
+                file={process.env.PUBLIC_URL + "./Report_hw2_bolon.pdf"}
+                onLoadSuccess={this.onDocumentLoadSuccess}
+            >
+              <Page pageNumber={pageNumber}/>
+            </Document>
+          </Col>
+          <Col sm={6}>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <div className="button">
+              <ButtonToolbar>
+                <Button onClick={() => this.prevPage()}>Prev</Button>
+                <Button onClick={() => this.nextPage()}>Next</Button>
+              </ButtonToolbar>
+            </div>
+
+            <br></br>
+
+
+            {this.state.alertFirst ?
+                (<Alert defaultShow={this.state.alertFirst} variant="info first">
+                  <Alert.Heading>You are already in the first page of the document</Alert.Heading>
+                  <p>To continue click of 'Prev'</p>
+                </Alert>) : ''
+            }
+
+
+            {this.state.alertLast ?
+                (<Alert variant="info last">
+                  <Alert.Heading>You are already in the last page of the document</Alert.Heading>
+                  <p>To continue click of 'Next'</p>
+                </Alert>) : ''
+            }
+
+            <br></br>
+
+            <p>Page {pageNumber} of {numPages}</p>
+
+            <br></br>
+
+
+            <p>Satisfaction level</p>
+            <ProgressBar now={now} label={`${now}%`} />
+
+          </Col>
+          </Row>
+        </Container>
+
+
+
     );
   }
 }
