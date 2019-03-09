1c71a1a763a234d2175efb559edad6f320eb8a6f
Natalie Bolon <natalie.bolonbrun@epfl.ch>
2019-03-09 11:32:18 +0100

    different buttons

---
+++ slideback/src/Comments.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {Button, Form} from "react-bootstrap";
+
+class Comments extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            textData: '',
+            comments: []
+        }
+
+    }
+
+    handleChange(event) {
+        this.setState({
+            textData: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const comments = this.state.comments;
+        comments.push(this.state.textData);
+        this.setState({
+            textData: '',
+            comments
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={e => this.handleSubmit(e)}>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Doubts</Form.Label>
+                        <Form.Control type="text" placeholder="Submit your doubts" value={this.state.textData}
+                        onChange={e => this.handleChange(e)}/>
+                    </Form.Group>
+                    <Button type="submit">Submit form</Button>
+
+                </Form>
+
+                <br></br>
+
+                <div className="comment">
+                    {this.state.comments.map(comment => {
+                        return <p>{comment}</p>
+                    })}
+                </div>
+            </div>
+
+
+
+
+        );
+    }
+}
+
+export default Comments;
