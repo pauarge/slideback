d6b7cfca7299ab342d23a8ef8a27e4b2cdcade9a
Pau Argelaguet <pau@outlook.com>
2019-03-09 01:22:00 +0100

    Adding socket

--- slideback/src/Receiver.js
+++ slideback/src/Receiver.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
+import io from 'socket.io-client';
+
 class Receiver extends Component {
+  constructor(props) {
+    super(props);
+
+    this.socket = null;
+  }
+
+  componentDidMount() {
+    this.socket = io('http://localhost:4000', { query: "mode=receiver" });
+  }
+
   render() {
     return (
       <p>Hello from receiver</p>
