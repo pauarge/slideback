94835c920ee6ef033c3355c0a0b469777e058a34
Natalie Bolon <natalie.bolonbrun@epfl.ch>
2019-03-09 13:20:07 +0100

    merging

--- slideback/src/Receiver.js
+++ slideback/src/Receiver.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import {Document, Page} from "react-pdf";
 
 import { SOCKET_URL } from './config';
-import { Col, Container, ProgressBar, Row} from "react-bootstrap";
+import {Button, Col, Container, ProgressBar, Row} from "react-bootstrap";
 
 import Comments from './Comments'
 
@@ -43,6 +43,57 @@ class Receiver extends Component {
   };
 
 
+  async translate() {
+
+  // Imports the Google Cloud client libraries
+  const vision = require('@google-cloud/vision').v1;
+
+// Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+// Bucket where the file resides
+  const bucketName = 'my-bucket';
+// Path to PDF file within bucket
+  const fileName = 'public/Report_hw2_bolon.pdf';
+
+  const gcsSourceUri = `gs://${bucketName}/${fileName}`;
+  const gcsDestinationUri = `gs://${bucketName}/${fileName}.json`;
+
+  const inputConfig = {
+    // Supported mime_types are: 'application/pdf' and 'image/tiff'
+    mimeType: 'application/pdf',
+    gcsSource: {
+      uri: gcsSourceUri,
+    },
+  };
+  const outputConfig = {
+    gcsDestination: {
+      uri: gcsDestinationUri,
+    },
+  };
+  const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
+  const request = {
+    requests: [
+      {
+        inputConfig: inputConfig,
+        features: features,
+        outputConfig: outputConfig,
+      },
+    ],
+  };
+
+  const [operation] = await client.asyncBatchAnnotateFiles(request);
+  const [filesResponse] = await operation.promise();
+  const destinationUri =
+      filesResponse.responses[0].outputConfig.gcsDestination.uri;
+  console.log('Json saved to: ' + destinationUri);
+}
+
+
+
 
   render() {
     const { pageNumber, numPages } = this.state;
@@ -67,7 +118,7 @@ class Receiver extends Component {
               <br></br>
               <br></br>
               <br></br>
-
+              <Button onclick={() => this.translate()}>halo</Button>
 
               <br></br>
               <Comments/>
